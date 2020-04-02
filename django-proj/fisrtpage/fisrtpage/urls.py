24c7fe57ce2d9143d649c10234ff797170730231
motvig31rus <motvig31@gmail.com>
2020-04-02 19:14:55 +0300

    first commit

---
+++ django-proj/fisrtpage/fisrtpage/urls.py
@@ -0,0 +1,26 @@
+"""fisrtpage URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path, include
+from flatpages import views
+from django.contrib import admin
+
+urlpatterns = [
+ path ('admin/', admin.site.urls),
+ path ('', views.home, name='admin'),
+ path ('hello/', views.home, name='hello'),
+ path ('aye/', views.home, name='aye')
+]
